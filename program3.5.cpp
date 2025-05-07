610ddae976442693e3541d6bb2e66ff2c10fc906
Fayan1818 <24ce122@charusat.edu.in>
2025-05-07 23:52:10 +0530

    Add files via upload

---
+++ program3.5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int superDigit(string n, int k) {
+    long long sum = 0;
+    for (char c : n)
+        sum += c - '0';
+    sum *= k;
+    if (sum < 10)
+        return sum;
+    return superDigit(to_string(sum), 1);
+}
+
+int main() {
+    string n;
+    int k;
+
+    cout << "Enter number string: ";
+    cin >> n;
+    cout << "Enter repeat count (k): ";
+    cin >> k;
+
+    cout << "Super Digit: " << superDigit(n, k) << endl;
+
+    cout << "\n24CE122_Fayan" << endl;
+    return 0;
+}
